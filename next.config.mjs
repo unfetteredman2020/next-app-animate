84249503ca026cdc5626b2c37e5aab29092fabe9
unfetteredman <weixingwang6032@dingtalk.com>
2024-03-05 17:20:26 +0800

    fix: eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    pageExtensions: [ 'jsx',  'tsx' , ],
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,96 @@
 import type { Config } from "tailwindcss";
 
+const SIZE = 2000;
+const RATIO = 3.75;
+
+const getListObject = (count = 0, flag?: boolean) => {
+  const object: Record<string, string> = {};
+  for (let i = 0; i < count; i++) {
+    object[`pc-${i}`] = `${i}px`;
+    object[`phone-${i}`] = flag && i <= 2 ? `${i}px` : `${i / RATIO}rem`;
+  }
+  return object;
+};
+
+const getZIndex = (list: number) => {
+  const object: Record<number, number> = {};
+  for (let i = 0; i < list; i++) {
+    object[i] = i;
+  }
+  return object;
+};
+
+const getScale = () => {
+  return Array.from({ length: 9999 }, (_, i) => i / 10).reduce((pre, cur) => {
+    pre["pc-" + cur] = cur + "";
+    return pre;
+  }, {} as Record<string, string>);
+};
+
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      spacing: {
+        ...getListObject(SIZE),
+      },
+      fontSize: {
+        ...getListObject(SIZE),
+      },
+      fontFamily: {
+        PingFangSC: ["PingFang SC", "PingFang"],
+        WixMadeforText: ["WixMadeforText"],
+      },
+      width: {
+        ...getListObject(SIZE),
+      },
+      maxWidth: {
+        ...getListObject(SIZE),
+      },
+      maxHeight: {
+        ...getListObject(SIZE),
+      },
+      minWidth: {
+        ...getListObject(SIZE),
+      },
+      minHeight: {
+        ...getListObject(SIZE),
+      },
+      height: {
+        ...getListObject(SIZE),
+      },
+      borderWidth: {
+        ...getListObject(SIZE),
+      },
+      padding: {
+        ...getListObject(SIZE),
+      },
+      margin: {
+        ...getListObject(SIZE),
+      },
+      borderRadius: {
+        ...getListObject(SIZE, true),
+      },
+      // 行高
+      lineHeight: {
+        ...getListObject(SIZE),
+      },
+      // 间距
+      gap: {
+        ...getListObject(SIZE),
+      },
+      // @ts-ignore
+      zIndex: {
+        ...getZIndex(SIZE),
+      },
+      inset: {
+        ...getListObject(SIZE),
+      },
+      scale: {
+        ...getScale(),
+      },
+      opacity: {
+        ...getListObject(SIZE),
       },
     },
   },
